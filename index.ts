40f00b464edb0ac267482c9d209e036dac68ccc5
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-15 01:42:48 +0700

    callback to show bot tag name

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ require('dotenv').config() // Recommended way of loading dotenv
 import container from './src/inversify.config'
 import { TYPES } from './src/types'
 import { Bot } from './src/bot'
+
 let bot = container.get<Bot>(TYPES.Bot)
 bot
   .listen()
-  .then(() => {
-    console.log('Logged in!')
+  .then((client) => {
+    console.log('Logged in with bot', client.user.tag)
   })
   .catch((error) => {
     console.log('Oh no! ', error)
