05a03a4580d8bd46e2e236311ed56bdc6200754a
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-14 23:56:03 +0700

    remove unused container bind

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,8 +4,6 @@ import { Container } from 'inversify'
 import { Client } from 'discord.js'
 import { TYPES } from './types'
 import { Bot } from './bot'
-import { MessageResponder } from './services/message-responder'
-import { PingFinder } from './services/ping-finder'
 
 config()
 
@@ -14,7 +12,5 @@ const container = new Container()
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope()
 container.bind<Client>(TYPES.Client).toConstantValue(new Client())
 container.bind<string>(TYPES.Token).toConstantValue(process.env.TOKEN || '')
-container.bind<MessageResponder>(TYPES.MessageResponder).to(MessageResponder).inSingletonScope()
-container.bind<PingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope()
 
 export default container
