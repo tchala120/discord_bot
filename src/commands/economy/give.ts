593b6d0388db49524128d348278d802789561402
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-17 21:37:07 +0700

    add command !give

---
+++ src/commands/economy/give.ts
@@ -0,0 +1,36 @@
+import { BitFieldResolvable, Message, PermissionString } from 'discord.js'
+import path from 'path'
+import {
+  addNewUserToDB,
+  isUserExist,
+  Profile,
+  retreiveProfileDB,
+  saveProfilesData,
+  toString,
+} from '../../../src/utils/user'
+
+export const execute = async (message: Message, args: string[]): Promise<Message> => {
+  const amount = Number(args[args.length - 1])
+  const target = message.mentions.users.first() || message.author
+
+  const dbPath = path.join(__dirname, '../../../', 'database/profiles.json')
+
+  let profiles: Profile[] = retreiveProfileDB(dbPath)
+
+  if (!isUserExist(target.id, profiles)) addNewUserToDB(target.id, target.tag, amount, dbPath, profiles)
+  else {
+    profiles = profiles.map((p) => ({ ...p, balance: p.id === target.id ? p.balance + amount : p.balance }))
+    saveProfilesData(dbPath, toString(profiles))
+  }
+
+  return message.channel.send(`มอบเหรียญให้กับ <@${target.id}> เป็นจำนวน ${amount} tcoin.`)
+}
+
+export const name: string = 'give'
+export const description: string = 'คำสั่งมอบเหรียญให้ user โดยคนที่ให้ต้องเป็น admin เท่านั้น'
+export const cooldown: number = 10
+export const icon: string = '🤲'
+export const permissions: BitFieldResolvable<PermissionString> = ['ADMINISTRATOR']
+export const roles: string[] = ['810852554358390814']
+export const args = true
+export const usage = '<user> <amount>'
