54b94362463090d7343d08cd2520b69db47bf6d1
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-17 22:13:35 +0700

    filter help commands by roles

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -3,21 +3,30 @@ import { ICommand } from '..'
 
 export const execute = async (
   message: Message,
-  args: any[],
+  args: string[],
   commands: Collection<string, ICommand>
 ): Promise<Message> => {
   const data: string[] = []
+  const myRoles = message.member.roles
 
-  console.log('args', args, 'commands', commands)
+  console.log('args', args)
 
   data.push('📚\t**คำสั่ง Help**\n')
-  data.push(commands.map((command) => `- \`${command.name}\` -->\t${command.description}\n`).join('\n'))
+  data.push(
+    commands
+      .map((command) => {
+        if (command.roles && command.roles.length > 0 && !myRoles.some((role) => command.roles?.includes(role.id)))
+          return ``
+        return `- \`${command.name}\` -->\t${command.description}\n\n`
+      })
+      .join('')
+  )
 
   return message.channel.send(data)
 }
 
 export const name: string = 'help'
-export const description: string = 'คำสั่งในการช่วยเหลือ เกี่ยวกับ "บอท" Tconomy.'
+export const description: string = 'คำสั่งในการช่วยเหลือ เกี่ยวกับ "บอท".'
 export const aliases: string[] = ['?']
 export const cooldown: number = 10
 export const icon: string = '📚'
