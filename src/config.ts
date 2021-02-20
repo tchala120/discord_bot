2232f2baf07ea935a2041602c480fe600ccac15c
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-02-21 06:00:26 +0700

    update config env to access to .env only

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,6 @@ export const TOKEN = process.env.BOT_TOKEN
 
 export const PREFIX = '!'
 
-export const CHANNELS = {
-  ECONOMY: '811571385649594429',
-  RULE: '811421236747436033',
-  WELCOME: '812080895136759808',
-}
-
 export const ROLES = {
   OWNER: '810852554358390814',
 }
